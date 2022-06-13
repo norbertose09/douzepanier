347d857095a18098d7489a314adff4b3d1630d7f
norbertose09 <91324184+norbertose09@users.noreply.github.com>
2022-06-13 09:17:24 -0400

    Update index.php

--- info/index.php
+++ info/index.php
@@ -73,7 +73,7 @@ The forex market is open 24 hours a day, five days a week, except for holidays. 
           </div>
         </div>
         <div class="col-lg-4 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="300">
-          <img src="../assets/img/undraw_business_plan_re_0v81.svg" class="img-fluid animated" alt="">
+          <img src="../assets/img/undraw_Business_plan_re_0v81.png" class="img-fluid animated" alt="">
         </div><br><br>
       </div>
     </div>
